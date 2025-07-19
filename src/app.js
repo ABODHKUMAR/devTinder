9bdebe9b4457be6c9af21c1037155b525da1d4ef
Abodh Shah <abodh.shah@gspann.com>
2025-07-19 21:44:11 +0530

    routing in express

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 // console.log("starting new project");
 const express = require('express');
 const app = express();
-app.use((req, res)=>{
-    res.send('Hello, World! at /');
+
+app.use("/hello/test",(req, res)=>{
+    res.send('hi hi hi!');
 })
 
-app.use("/hello",(req,res)=>{
-    res.send('Hello, Hello');
+app.use("/hello",(req, res)=>{
+    res.send('hello hello hello!');
 })
 
-app.use("/test",(req, res)=>{
-    res.send('Hello, World!');
+
+app.use("/test",(req,res)=>{
+    res.send('hello from the server!');
 })
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+
+app.use("/",(req, res)=>{
+    res.send('Namaste All!');
+})
+
+
+app.listen(7777, () => {
+  console.log('Server is running on port 7777...');
 });
