3c31381acbe3fafa544de818cfcba38795521985
Abodh Shah <abodh.shah@gspann.com>
2025-07-19 22:50:57 +0530

    paying with routes

--- src/app.js
+++ src/app.js
@@ -2,42 +2,84 @@
 const express = require('express');
 const app = express();
 
-app.use("/user", (req, res, next) => {
-    console.log("Middleware for /user");
-    res.send({
-      message: "Hello from the middleware!"
-    });
-});
+// regex route matching example
+// app.get(/a/, (req, res) => {
+//   res.send('Hello, World!');
+//   console.log("Received a GET request to /a/");
+// });
 
-//This will only handle GET call to '/user'
-app.get("/user",(req,res)=>{
-    res.send({
-      name: "John Doe",
-      age: 30,
-      email: "john.doe@example.com"
-    });
-})
+// // it will match anystring that end with "fly"
+// app.get(/.%fly$/, (req, res) => {
+//   res.send('Hello, World! with regex ending');
+//   console.log("Received a GET request to /.%fly$/");
+// });
 
-app.post("/user", (req, res) => {
-    console.log("Received a POST request to /user");
-    res.send({  
-      message: "User created successfully!"
-    }); 
-});
-
-app.delete("/user", (req, res) => {
-    console.log("Received a DELETE request to /user");
-    res.send({
-      message: "User deleted successfully!"
-    });
-});
-
-// this will match all the HTTP methods API calls to /test
-app.use("/test",(req,res)=>{
-    res.send('hello from the server!');
-})
+// // it will match "abc" or "ac"
+// app.get("/ab?c", (req, res) => {
+//   res.send('Hello, World!');
+// });
 
 
+// // it will match "abd" or "ac"
+// app.get("/a(bd)?c", (req, res) => {
+//   res.send('Hello, World! with optional b');
+//   console.log("Received a GET request to /a(bd)?c");
+// });
+
+
+// // it will match "abbbc" or "ac"
+// app.get("/ab+c", (req, res) => {
+//   res.send('Hello, World! with one or more b');
+//   console.log("Received a GET request to /ab+c");
+// });
+
+
+// // it will match "ac" or "abbbc" or "abbbbbc"
+// app.get("/ab*c", (req, res) => {
+//   res.send('Hello, World! with zero or more b');
+//   console.log("Received a GET request to /ab*c");
+// });
+
+//http://0.0.0.0:7777/user?id=12322&password=testing // query parameters
+// app.get("/user", (req, res) => {
+//   const userId = req.query.id;
+//   console.log(req.query)
+//   if (userId) {
+//     console.log(`Received a GET request to /user with id: ${userId}`);
+//   } else {
+//     console.log("Received a GET request to /user without id");
+//   } 
+//   res.send('Hello, World! with query parameter');
+//   console.log("Received a GET request to /user?id=12345");
+// });
+
+//http://0.0.0.0:7777/user/12345 // params
+// app.get("/user/:id", (req, res) => {
+//   const userId = req.params.id;
+//   console.log(req.params)
+//   if (userId) {
+//     console.log(`Received a GET request to /user with id: ${userId}`);
+//   } else {
+//     console.log("Received a GET request to /user without id");
+//   } 
+//   res.send('Hello, World! with path parameter');
+//   console.log("Received a GET request to /user/12345");
+// });
+
+
+app.get("/user/:id/:name", (req, res) => {
+  const userId = req.params.id;
+  const userName = req.params.name;
+  console.log(req.params);
+  if (userId && userName) {
+    console.log(`Received a GET request to /user with id: ${userId} and name: ${userName}`);
+  }
+  else {
+    console.log("Received a GET request to /user without id or name");
+  }
+  res.send(`Hello, ${userName}! Your user ID is ${userId}.`);
+  console.log(`Received a GET request to /user/${userId}/${userName}`);
+} );
 
 app.listen(7777, () => {
   console.log('Server is running on port 7777...');
