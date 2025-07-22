72dab21a3886b15d6a65ce2806ff779466d6c28b
Abodh Shah <abodh.shah@gspann.com>
2025-07-22 08:45:11 +0530

    error handling

--- src/app.js
+++ src/app.js
@@ -1,37 +1,33 @@
 const express = require('express');
 const app = express();
-const {adminAuth, userAuth} = require('./middlewares/auth');
-//handle Auth Middleware for all requests, all, GET, POST, PUT, DELETE
-app.use("/admin", adminAuth);
-app.use("/user", userAuth);
 
 
-app.get("/admin/getAllData", (req, res) => {
-    //logic to fetching all data from the database
-  // if the request is authorized, proceed to fetch data
-  // const token = "xyz123"; 
-  // const isAdminAuthorized =  token === "xyz123"; // Example token check
-  // if (isAdminAuthorized) {
-  //   res.send("All Data Fetched");
-  // } else {
-  //   res.status(401).send("Unauthorized request");
-  // }
-
-  res.send("All Data Fetched");
-
-
-});
-
-app.get("/admin/deleteUser", (req, res) => {
-  //logic to delete a user from the database
-  res.send("User Deleted");
-});
-
-app.get("/user", (req, res) => {
+app.get("/getUserData", (req, res) => {
   //logic to fetch user data
+  // throw new Error("Error fetching user data");
   res.send("User Data Fetched");
 });
 
+app.use("/",(err, req, res, next)=>{
+    if(err){
+      console.error("Error occurred:", err.message);
+      res.status(500).send("Internal Server Error");
+    }
+});
+
+app.use("/products", (req, res,) => {
+  // logic to fetch products
+  try {
+    throw new Error("Error fetching products");
+    
+  } catch (error) {
+    console.error("Error fetching products:", error.message);
+    res.status(500).send("Internal Server Error");
+    return;
+    
+  }
+  res.send("Products Fetched");
+});
 app.listen(7777, () => {
   console.log('Server is running on port 7777...');
 })
