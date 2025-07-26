d5b0d726a209a00608336e9fb65ae80cd3a30852
Abodh Shah <abodh.shah@gspann.com>
2025-07-26 17:18:19 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -52,6 +52,36 @@ app.get("/feed", async (req, res) => {
   }
 });
 
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    if (!user) {
+      return res.status(404).json({ message: "No user found" });
+    }
+    res.status(200).json({ message: "User deleted successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting user", error: error.message });
+  }
+});
+
+//update the data of the user
+app.patch("/user", async (req, res) => {
+  const data = req.body;
+  const userId = data.userId;
+
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, { returnDocument: "after", runValidators: true });
+    if (!user) {
+      return res.status(404).json({ message: "No user found" });
+    }
+    res.status(200).json({ message: "User updated successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating user", error: error.message });
+  }
+});
+
 connectDB()
   .then(() => {
     app.listen(7777, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
    firstName: {
         type: String,
-        required: true
+        required: true,
+        minlength: 4,
+        maxlength: 40
    },
     lastName: {
           type: String,
-          required: true
+          required: true,
+          minlength: 4,
+          maxlength: 40
     },
     emailId: {
         type: String,
@@ -22,13 +26,32 @@ const userSchema = new mongoose.Schema({
     },
     age: {
         type: Number,
-        min: 0,
+        min: 18,
         max: 120
     },
     gender : {
         type: String,
-        enum: ['male', 'female', 'other']
+        validate: function(value) {
+            if(['male', 'female', 'other'].includes(value)) {
+                return true;
+            }
+            throw new Error(`${value} is not a valid gender!`);
+        }
+    },
+    photoUrl: {
+        type: String,
+        default: 'https://shorturl.at/CjByg'
+    },
+    about: {
+        type: String,
+        default: 'This is a default about of the user!'
+    },
+    skills:{
+        type: [String],
+        default: []
     }
+},{
+    timestamps: true,
 });
 //User is a model for the user collection in the database
 const User  = mongoose.model('User', userSchema);
